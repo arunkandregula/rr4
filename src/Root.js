956b0d9f5de66e390764a3a2c7ed6bbdfb5bfda0
Arun Kandregula <kandregula.arunkumar@gmail.com>
2020-11-18 16:12:09 -0800

    Upgraded libraries

--- src/Root.js
+++ src/Root.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'; // react-router for browser
-import Home from 'components/Home';
-import About from 'components/About';
-import Contact from 'components/Contact';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 
 
